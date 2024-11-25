285f8ed0d0650dda94026807dfb426a50f9530f2
codeWithThunder <atikulla345@gmail.com>
2024-11-25 22:40:51 +0600

    about page update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { FaHtml5 } from "react-icons/fa";
 import { HiPhoto } from "react-icons/hi2";
@@ -9,6 +13,13 @@ import { IoRocket } from "react-icons/io5";
 
 import image from "../image/about/info-img.jpg";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+
+import { GoDotFill } from "react-icons/go";
+
+import user1 from "../image/Testimonial/img1.jpg";
+import user2 from "../image/Testimonial/img2.jpg";
+import user3 from "../image/Testimonial/img3.jpg";
+import user4 from "../image/Testimonial/img4.jpg";
 
 const About = (props) => {
   const [text] = useTypewriter({
@@ -54,6 +65,40 @@ const About = (props) => {
       icon: <IoRocket />,
       tittle: "Branding",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+  ];
+  const TestimonialClint = [
+    {
+      id: 1,
+      name: "Julia Sakura",
+      description:
+        "Russell did an excellent creative job, addressing our request quickly, and also providing her own graphic insight, being open to feedback and changes or edits when they arose. She worked with us the entire way. Highly recommended.",
+      position: "Envato Customer",
+      image: user1,
+    },
+    {
+      id: 2,
+      name: "John Santana",
+      description:
+        "Russell did an excellent creative job, addressing our request quickly, and also providing her own graphic insight, being open to feedback and changes or edits when they arose. She worked with us the entire way. Highly recommended.",
+      position: "Envato Customer",
+      image: user2,
+    },
+    {
+      id: 3,
+      name: "Maria Wilson",
+      description:
+        "Russell did an excellent creative job, addressing our request quickly, and also providing her own graphic insight, being open to feedback and changes or edits when they arose. She worked with us the entire way. Highly recommended.",
+      position: "Envato Customer",
+      image: user3,
+    },
+    {
+      id: 4,
+      name: "Emma Smith",
+      description:
+        "Russell did an excellent creative job, addressing our request quickly, and also providing her own graphic insight, being open to feedback and changes or edits when they arose. She worked with us the entire way. Highly recommended.",
+      position: "Envato Customer",
+      image: user4,
     },
   ];
 
@@ -187,6 +232,8 @@ const About = (props) => {
 
       {/* My Services */}
       <MyServices MyServicesData={MyServicesData} />
+
+      <Testimonial TestimonialClint={TestimonialClint} />
     </main>
   );
 };
@@ -232,5 +279,134 @@ const MyServices = (props) => {
     </div>
   );
 };
+
+const Testimonial = (props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+        },
+      },
+    ],
+    appendDots: (dots) => (
+      <div style={{ bottom: "-40px" }}>
+        <ul className="m-0 " id="bannerDotId">
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: () => (
+      <div className="bannerDotliId w-[17px] bottom-0">
+        <GoDotFill className="cursor-pointer [@media(min-width:640px)]:text-[17px] text-[16px]" />
+      </div>
+    ),
+  };
+
+  return (
+    <div className="text-[white] body-font bg-[#101010] GeologicaFont pt-[1rem] pb-[2rem]">
+      <div className="xl:w-[1230px] w-[100%] [@media(min-width:460px)]:px-5 px-[5px] mx-auto pb-[50px] relative ">
+        <p className="text-[#03BFFF] [@media(min-width:530px)]:text-[15px] [@media(min-width:460px)]:text-[13px] text-[12px] mx-auto  text-center relative z-10">
+          Testimonial
+        </p>
+        <h1 className="[@media(min-width:640px)]:text-3xl [@media(min-width:460px)]:text-[26px] text-[20px] [@media(min-width:640px)]:leading-[36px] [@media(min-width:460px)]:leading-[32px] leading-[25px] font-semibold title-font [@media(min-width:460px)]:mb-[19px] [@media(min-width:640px)]:mb-[1.5rem] mb-[0px] text-center relative z-10">
+          What People Say
+          <br />
+          About Me
+        </h1>
+        <div className=" [@media(min-width:846px)]:py-[2px] py-[10px] mx-auto flex items-center w-[100%] relative z-10 Poppins">
+          <Slider {...settings} className="w-full h-full">
+            {props.TestimonialClint.map((key) => {
+              return (
+                <div
+                  key={key.id}
+                  className="[@media(min-width:640px)]:px-[10px] px-0 w-[100%] my-[10px]"
+                >
+                  <div className="h-full [@media(min-width:460px)]:bg-[#141414] bg-none [@media(min-width:640px)]:px-8 [@media(min-width:460px)]:px-6 px-[5px] [@media(min-width:640px)]:py-8 py-4 rounded [@media(min-width:460px)]:shadow-[0px_0px_3px_0px_#0000008c] shadow-none [@media(min-width:460px)]:text-left text-center">
+                    <div className="flex [@media(min-width:460px)]:flex-row flex-col [@media(min-width:460px)]:justify-between justify-center items-center [@media(min-width:640px)]:mb-6 mb-4">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        className="[@media(min-width:460px)]:hidden flex [@media(min-width:460px)]:w-[40px] w-[2rem] text-[#fff] [@media(min-width:460px)]:mx-0 mx-auto"
+                        viewBox="0 0 975.036 975.036"
+                      >
+                        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                      </svg>
+                      <div className="[@media(min-width:460px)]:flex hidden items-center ">
+                        <img
+                          alt="testimonial"
+                          src={key.image}
+                          className="[@media(min-width:640px)]:w-12 w-[35px] [@media(min-width:640px)]:h-12 h-[35px] rounded-full flex-shrink-0 object-cover object-center"
+                        />
+                        <span className="flex-grow flex flex-col [@media(min-width:640px)]:pl-4 [@media(min-width:460px)]:pl-[13px] pl-[10px] [@media(min-width:640px)]:leading-[23px] leading-[17px] text-left">
+                          <span className="[@media(min-width:460px)]:font-medium font-normal [@media(min-width:640px)]:text-[15px] text-[14px] text-[#efefef]">
+                            {key.name}
+                          </span>
+                          <span className="[@media(min-width:640px)]:text-[15px] [@media(min-width:460px)]:text-[12px] text-[11px]  font-[400] [@media(min-width:460px)]:ml-0 ml-[1.5px] text-[#9f9f9f] pt-[3px]">
+                            {key.position}
+                          </span>
+                        </span>
+                      </div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        className="[@media(min-width:460px)]:flex hidden [@media(min-width:460px)]:w-[40px] w-[2rem] text-[#fff] [@media(min-width:460px)]:mx-0 mx-auto"
+                        viewBox="0 0 975.036 975.036"
+                      >
+                        <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+                      </svg>
+                    </div>
+
+                    <p className="[@media(min-width:640px)]:text-[15px] text-[13px] font-[400] text-[#9f9f9f] [@media(min-width:460px)]:leading-[1.95] leading-[1.55]">
+                      {key.description}
+                    </p>
+                    <div className="[@media(min-width:460px)]:hidden flex justify-center w-full pt-[1.5rem]" >
+                    <div className="flex items-center ">
+                        <img
+                          alt="testimonial"
+                          src={key.image}
+                          className="[@media(min-width:640px)]:w-12 w-[35px] [@media(min-width:640px)]:h-12 h-[35px] rounded-full flex-shrink-0 object-cover object-center"
+                        />
+                        <span className="flex-grow flex flex-col [@media(min-width:640px)]:pl-4 [@media(min-width:460px)]:pl-[13px] pl-[10px] [@media(min-width:640px)]:leading-[23px] leading-[17px] text-left">
+                          <span className="[@media(min-width:460px)]:font-medium font-normal [@media(min-width:640px)]:text-[15px] text-[14px] text-[#efefef]">
+                            {key.name}
+                          </span>
+                          <span className="[@media(min-width:640px)]:text-[15px] [@media(min-width:460px)]:text-[12px] text-[11px]  font-[400] [@media(min-width:460px)]:ml-0 ml-[1.5px] text-[#9f9f9f] pt-[3px]">
+                            {key.position}
+                          </span>
+                        </span>
+                      </div>
+                    </div>
+                    
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function SampleNextArrow() {
+  return <div className="hidden"></div>;
+}
+function SamplePrevArrow() {
+  return <div className="hidden"></div>;
+}
 
 export default About;
